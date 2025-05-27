c05c4370cde801dd85baa0bb7ee6584ac8e789e8
josefinaPorolli <j.porolli@alumno.um.edu.ar>
2025-05-27 09:34:43 -0300

    Excepciones: creación de excepciones

---
+++ src/exceptions.py
@@ -0,0 +1,18 @@
+class PacienteNoExisteError(Exception):
+    def __init__(self, dni: str):
+        super().__init__(f'El paciente con DNI {dni} no existe.')
+
+
+class MedicoNoExisteError(Exception):
+    def __init__(self, matricula: str):
+        super().__init__(f'El médico con matrícula {matricula} no existe.')
+
+
+class TurnoEnElPasadoError(Exception):
+    def __init__(self):
+        super().__init__('La fecha y hora del turno no puede ser en el pasado.')
+
+
+class TurnoDuplicadoError(Exception):
+    def __init__(self, fecha_hora):
+        super().__init__(f'El médico ya tiene un turno asignado en ese horario ({fecha_hora}).')
